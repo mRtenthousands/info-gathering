b46faa6e68dd980941e6e0b88144712524cc3b02
Zihao Wan <z.wan0220241.@arts.ac.uk>
2025-04-21 16:45:50 +0100

    添加项目文件。

---
+++ 自造计算机/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 信息收集
+{
+    internal class Program
+    {
+        static void Main()
+        {   
+            //名称收集
+            Console.WriteLine("Please enter your name");
+            string name = Console.ReadLine();//string 类型收集
+
+            //年龄收集
+            Console.WriteLine("Please enter your age");
+            string age = Console.ReadLine();//创建库来保存输入的信息
+
+            //家乡收集
+            Console.WriteLine("Please enter your hometown");
+            string address = Console.ReadLine();
+
+            //收集爱好
+            Console.WriteLine("Please enter your hobbies");
+            string hobbies = Console.ReadLine();
+
+            //组织格式
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Your name is:" + name);
+            Console.WriteLine("Your age is:" + age);
+            Console.WriteLine("Your hometown is:" + address);
+            Console.WriteLine("Your bobbies is:" + hobbies);
+
+            Console.ReadKey(); 
+        }
+    }
+}
